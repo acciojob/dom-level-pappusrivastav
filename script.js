ea66370933ca1032a4dbd54d402a275767ea4ba2
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-05 19:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const targetElement = document.getElementById("level");
+  let domLevel = 0;
+  let currentElement = targetElement;
+
+  while (currentElement !== document.documentElement) {
+    domLevel++;
+    currentElement = currentElement.parentElement;
+  }
+
+  alert(`The level of the element is: ${domLevel}`);
+});
